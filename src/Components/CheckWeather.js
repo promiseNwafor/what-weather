b46b0bf4c4dc774080a36e3255823798f165fa48
Promise Nwafor <makybeky9@gmail.com>
2020-07-29 21:33:18 +0100

    first

---
+++ src/Components/CheckWeather.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CheckWeather() {
+  return (
+    <div className="CheckWeather">
+      <h1>Api Call</h1>
+    </div>
+  );
+}
+
+export default CheckWeather;   
---
+++ src/Components/DisplayWeather.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function DisplayWeather() {
+  return (
+    <div className="DisplayWeather">
+      <h1>Result Display</h1>
+    </div>
+  );
+}
+
+export default DisplayWeather;
